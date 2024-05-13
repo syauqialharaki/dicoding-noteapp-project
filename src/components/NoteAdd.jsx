cd8cb9531759f24502f9ac4643400fabb4a65028
musyaffaas <syauqialharaki@gmail.com>
2024-05-13 17:45:22 +0700

    Added fitur tambah

--- src/components/NoteAdd.jsx
+++ src/components/NoteAdd.jsx
@@ -1,30 +1,74 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function NoteAdd() {
-  return (
-    <>
-      <div className="w-full p-10 mt-8 mb-8 border border-gray-300 rounded-lg">
-        <div>
-          <div className="flex items-center mb-2">
-            <div className="bg-green-500 text-white text-xl px-2 py-1 rounded mr-2">
-              aku
-            </div>
-            <h3 className="text-2xl font-semibold">tes</h3>
-          </div>
-          <p className="text-xl mb-4 overflow-hidden">aa</p>
-        </div>
-      </div>
-      <div className="flex justify-center mb-8">
-        <Link
-          to={`/`}
-          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 mr-4 rounded"
-        >
-          Kembali
-        </Link>
-      </div>
-    </>
-  );
-}
+class NoteAdd extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      title: "",
+      body: "",
+    };
+
+    this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this);
+    this.onBodyChangeEventHandler = this.onBodyChangeEventHandler.bind(this);
+
+    this.onSubmitChangeEventHandler =
+      this.onSubmitChangeEventHandler.bind(this);
+  }
+
+  onTitleChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        title: event.target.value,
+      };
+    });
+  }
+
+  onBodyChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        body: event.target.value,
+      };
+    });
+  }
+
+  onSubmitChangeEventHandler(event) {
+    event.preventDefault();
+    this.props.addNote(this.state);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmitChangeEventHandler}>
+        <div className="bg-white  rounded-lg px-6 py-4 flex flex-col h-full">
+          <input
+            type="text"
+            placeholder="Judul"
+            value={this.state.title}
+            onChange={this.onTitleChangeEventHandler}
+            className="text-xl border rounded-lg px-6 py-4 font-semibold mb-2 text-center outline-none"
+          />
+          <textarea
+            placeholder="Isi catatan"
+            value={this.state.body}
+            onChange={this.onBodyChangeEventHandler}
+            className="mb-2 border rounded-lg px-6 py-4 overflow-hidden text-center resize-none outline-none flex-grow"
+            style={{
+              WebkitLineClamp: "3",
+              WebkitBoxOrient: "vertical",
+              display: "-webkit-box",
+            }}
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+          >
+            Tambah
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
 export default NoteAdd;
--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import { addNote } from "../utils/local-data";
 import NoteAdd from "../components/NoteAdd";
 
-function Add() {
-  function onAddNotesHandler(note) {
-    addNote(note);
+class Add extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onAddNotesHandler = this.onAddNotesHandler.bind(this);
   }
 
-  return (
-    <>
+  onAddNotesHandler({ title, body }) {
+    addNote({ title, body });
+  }
+  render() {
+    return (
       <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <NoteAdd addNote={onAddNotesHandler} />
+        <NoteAdd addNote={this.onAddNotesHandler} />
       </section>
-    </>
-  );
+    );
+  }
 }
 export default Add;
